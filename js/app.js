c7747a98141de29050b313d20c093858352edf41
discloseagency <68660620+discloseagency@users.noreply.github.com>
2020-07-22 15:16:49 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const listenToClickOnMenu = document.querySelector('.container-clicked');
+const spinMenuOnClick = document.querySelector('.container');
+const toggleMenu = document.querySelector('.container-menu');
+
+const body = document.querySelector('body');
+
+body.addEventListener('click', function(event){
+    let regex = /container-menu-item/g;
+    if (event.target.classList[0] == "container-menu-item") {
+        toggleMenu.classList.toggle('display-none');
+    }
+});
+
+listenToClickOnMenu.addEventListener('click', function(event) {
+    event.preventDefault();
+    toggleMenu.classList.toggle('display-none');
+    toggleMenu.classList.toggle('scale-up-top');
+    spinMenuOnClick.classList.toggle('container-active-in');
+});
