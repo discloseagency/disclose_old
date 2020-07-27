abb781b0a21daa63bcbe88eb1c71ffca6d7ae3bc
Michel Marechal <michelm.marechal@gmail.com>
2020-07-27 20:45:54 -0300

    add lazyload

--- js/app.js
+++ js/app.js
@@ -25,17 +25,24 @@ const year = date.getFullYear();
 const addYear = document.querySelector('.year');
 addYear.innerText += year;
 
-// Google Maps
-
-console.log("hue");
-
-const mapParent = document.querySelector('.our-location');
-
+// Lazy Load
 function lazyLoad() {
+    const mapParent = document.querySelector('.our-location');
     const mapHTMLElement = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3392663071804!2d-46.665133784470136!3d-23.556255684685322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59dd6a6afd61%3A0x6a2e6f872e2ae1aa!2sWeWork%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1595426344159!5m2!1spt-BR!2sbr" width="100%" height="600px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
+    let cards = document.querySelectorAll('.card');
+    const cardImages = ["pr", "influencers", "content-marketing"];
+
+    setTimeout(function() {
+        let index = 0;
+        cards.forEach((card) => {
+            card.classList.add(cardImages[index]);
+            index++;
+        });
+    }, 1000)
+
     setTimeout(function() {
         mapParent.innerHTML += mapHTMLElement;
-    }, 1000);
+    }, 2000);
 }
 
 lazyLoad();
