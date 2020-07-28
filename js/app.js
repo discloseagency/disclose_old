7309823280f10cf853136e1f866a71df6e3f2c0d
Michel Marechal <michelm.marechal@gmail.com>
2020-07-28 03:03:41 -0300

    try to fix map

--- js/app.js
+++ js/app.js
@@ -4,26 +4,34 @@ const toggleMenu = document.querySelector('.container-menu');
 
 const body = document.querySelector('body');
 
+function toggleMenuVisibility() {
+    toggleMenu.classList.toggle('display-none');
+    toggleMenu.classList.toggle('scale-up-top');
+    spinMenuOnClick.classList.toggle('container-active-in');
+}
 
 body.addEventListener('click', function(event){
     let regex = /container-menu-item/g;
     if (event.target.classList[0] == "container-menu-item") {
-        toggleMenu.classList.toggle('display-none');
+        toggleMenuVisibility();
     }
 }, {passive: true});
 
 listenToClickOnMenu.addEventListener('click', function(event) {
-    toggleMenu.classList.toggle('display-none');
-    toggleMenu.classList.toggle('scale-up-top');
-    spinMenuOnClick.classList.toggle('container-active-in');
-}, {passive: true});
+    event.preventDefault();
+    toggleMenuVisibility();
+    
+}, {passive: false});
 
 
-const date = new Date();
-const year = date.getFullYear();
+function newYear() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const addYear = document.querySelector('.year');
+    addYear.innerText += year;
+}
 
-const addYear = document.querySelector('.year');
-addYear.innerText += year;
+newYear();
 
 // Lazy Load
 function lazyLoad() {
