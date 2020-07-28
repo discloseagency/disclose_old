5e3b4e9cde41c1801c3a4e5cad344252cb665f22
Michel Marechal <michelm.marechal@gmail.com>
2020-07-28 14:58:55 -0300

    change location

--- js/app.js
+++ js/app.js
@@ -37,7 +37,7 @@ newYear();
 function lazyLoad() {
     const mapParent = document.querySelector('.our-location');
     const authorImage = document.querySelector('.rounded-img');
-    const mapHTMLElement = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.3392663071804!2d-46.665133784470136!3d-23.556255684685322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59dd6a6afd61%3A0x6a2e6f872e2ae1aa!2sWeWork%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1595426344159!5m2!1spt-BR!2sbr" width="100%" height="600px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
+    const mapHTMLElement = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.390504581313!2d-46.68937528492469!3d-23.55441448468626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce579c1e66ce89%3A0x74b68881cb73c25c!2sGarage%20Coworking!5e0!3m2!1spt-BR!2sbr!4v1595958986022!5m2!1spt-BR!2sbr" width="100%" height="600px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`
     let cards = document.querySelectorAll('.card');
     const cardImages = ["pr", "influencers", "content-marketing"];
 
